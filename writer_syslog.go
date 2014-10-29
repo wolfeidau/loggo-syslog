b0826337073d9734d535b904338508c981c3d394
Mark Wolfe <mark@wolfe.id.au>
2014-10-29 13:38:38 +1100

    Thunderbirds are go!

---
+++ writer_syslog.go
@@ -0,0 +1,74 @@
+package lsyslog
+
+import (
+	"fmt"
+	"path/filepath"
+	"time"
+
+	"github.com/hashicorp/go-syslog"
+	"github.com/juju/loggo"
+)
+
+var formatter = &SyslogFormatter{}
+
+// SyslogFormatter provides a simple concatenation of message components.
+type SyslogFormatter struct{}
+
+// Format returns the parameters separated by spaces except for filename and
+// line which are separated by a colon.
+func (*SyslogFormatter) Format(level loggo.Level, module, filename string, line int, timestamp time.Time, message string) string {
+
+	// Just get the basename from the filename
+	filename = filepath.Base(filename)
+	return fmt.Sprintf("%s %s:%d %s", module, filename, line, message)
+}
+
+type syslogWriter struct {
+	syslogger gsyslog.Syslogger
+}
+
+// NewSyslogWriter returns a new writer that writes
+// log messages to syslog in a simple format tailored for syslog
+func NewSyslogWriter(p gsyslog.Priority, facility, tag string) loggo.Writer {
+	syslogger, err := gsyslog.NewLogger(p, facility, tag)
+	if err != nil {
+		panic(err)
+	}
+	return &syslogWriter{syslogger}
+}
+
+// NewDefaultSyslogWriter returns a new writer that writes
+// log messages to syslog in a simple format tailored for syslog.
+// Note this defaults to using LOCAL7 facility which is pretty common.
+func NewDefaultSyslogWriter(level loggo.Level, tag string) loggo.Writer {
+	syslogger, err := gsyslog.NewLogger(convertLevel(level), "LOCAL7", tag)
+	if err != nil {
+		panic(err) // of course this will never happen
+	}
+	return &syslogWriter{syslogger}
+}
+
+func (slog *syslogWriter) Write(level loggo.Level, module, filename string, line int, timestamp time.Time, message string) {
+	logLine := formatter.Format(level, module, filename, line, timestamp, message)
+	slog.syslogger.WriteLevel(convertLevel(level), []byte(logLine))
+}
+
+func convertLevel(level loggo.Level) gsyslog.Priority {
+	switch level {
+	case loggo.DEBUG:
+		return gsyslog.LOG_DEBUG
+	case loggo.INFO:
+		return gsyslog.LOG_INFO
+	case loggo.WARNING:
+		return gsyslog.LOG_WARNING
+	case loggo.CRITICAL:
+		return gsyslog.LOG_CRIT
+	case loggo.ERROR:
+		return gsyslog.LOG_ERR
+	case loggo.TRACE:
+		return gsyslog.LOG_DEBUG
+	default:
+		return gsyslog.LOG_DEBUG
+	}
+
+}
---
+++ writer_syslog_test.go
@@ -0,0 +1,35 @@
+package lsyslog
+
+import (
+	"testing"
+
+	gc "gopkg.in/check.v1"
+
+	"github.com/juju/loggo"
+)
+
+func Test(t *testing.T) {
+	gc.TestingT(t)
+}
+
+type loggerSuite struct{}
+
+var _ = gc.Suite(&loggerSuite{})
+
+func (*loggerSuite) SetUpTest(c *gc.C) {
+	loggo.ResetLoggers()
+	loggo.ResetWriters()
+}
+
+// note this is really just a smoke test at the moment.
+func (*loggerSuite) TestSyslogWriter(c *gc.C) {
+	defaultWriter, level, err := loggo.RemoveWriter("default")
+	c.Assert(err, gc.IsNil)
+	c.Assert(level, gc.Equals, loggo.TRACE)
+	c.Assert(defaultWriter, gc.NotNil)
+
+	err = loggo.RegisterWriter("default", NewDefaultSyslogWriter(loggo.INFO, ""), loggo.INFO)
+	c.Assert(err, gc.IsNil)
+
+	loggo.GetLogger("").Infof("some test %s", "hello")
+}
